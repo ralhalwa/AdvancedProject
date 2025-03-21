8e1d25a96f81706ebc5077734b7aaa524a1a69b0
a-awadhi <awadhiahmed009@gmail.com>
2025-03-21 20:24:08 +0300

    Commit

--- FormApp/obj/Debug/net8.0-windows/FormApp.AssemblyInfo.cs
+++ FormApp/obj/Debug/net8.0-windows/FormApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46dc7cd841b45d142dd9327dda6dc4d158b8c114")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d80197a1c8da7f074b577b02c5441f3b2d7013e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
